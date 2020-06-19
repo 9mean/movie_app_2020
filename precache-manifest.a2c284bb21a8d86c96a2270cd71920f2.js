bbef20b16cd3df44d7ee077c74b885ed45d62a82
unknown <17011602@sju.ac.kr>
2020-06-19 21:31:15 +0900

    Updates

---
+++ precache-manifest.a2c284bb21a8d86c96a2270cd71920f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b30304c9e3490c3a422b7af20dbc75",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7ff0e24e3a320b246c39",
+    "url": "/movie_app_2020/static/css/main.63a1068b.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff0e24e3a320b246c39",
+    "url": "/movie_app_2020/static/js/main.0d17d68d.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
